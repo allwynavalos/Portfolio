52d683126de0adbc3fb1753de911bc76131e7a47
yourusername <allwynavalos@Allwyns-iMac.local>
2021-08-26 15:46:38 -0400

    deploy

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const base = process.env.NODE_ENV === "production" ? "deployed URL" : "http://localhost:3000"
+const base = process.env.NODE_ENV === "production" ? "https://git.heroku.com/ollie-avalos.git" : "http://localhost:3000"
 
 const api = axios.create({
   baseURL: base
